e57e9b5c0da05869474150521d7c1037dcb90363
meet-korat <meetkorat1406@gmail.com>
2026-01-07 23:15:55 +0530

    Homepage updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import styled, { ThemeProvider } from "styled-components";
 import { darkTheme } from "./utils/Theme.js";
-import {Home} from "./pages/Home.jsx";
+import Home from "./pages/Home.jsx";
 import {CreatePost} from "./pages/CreatePost";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
